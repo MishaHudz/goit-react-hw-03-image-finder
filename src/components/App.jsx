a4a76b079afbd4d35fb5fd18c4960455bcb1a9ab
MishaHudz <misha.hudz@gmail.com>
2023-05-31 00:34:07 +0300

    finished task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,78 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { fetchImages } from 'services/images-api';
+
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ButtonLoadMore } from 'components/Button/Button';
+import { Loader } from './Loader/Loader';
+import { Modal } from './Modal/Modal';
+
+export class App extends Component {
+  state = {
+    imageList: [],
+    filter: '',
+    page: 1,
+    loading: false,
+    totalHits: 0,
+    image: '',
+  };
+
+  componentDidUpdate(_, prevState) {
+    const { filter, page } = this.state;
+
+    if (prevState.filter !== filter || prevState.page !== page) {
+      this.setState({ loading: true });
+      this.getImages(page, filter);
+    }
+  }
+
+  getImages = async (page, inputedString) => {
+    const {
+      data: { hits, totalHits },
+    } = await fetchImages(inputedString, page);
+    this.setState({ loading: false });
+    this.setState(prev => ({
+      totalHits,
+      imageList:
+        prev.imageList.length !== 0 ? [...prev.imageList, ...hits] : hits,
+    }));
+  };
+
+  changePage = () => {
+    this.setState(prev => ({ page: prev.page + 1 }));
+  };
+
+  formSubmit = inputedString => {
+    if (this.state.filter === inputedString) return;
+    this.setState({ filter: inputedString, page: 1, imageList: [] });
+  };
+
+  showBigImg = bigImg => {
+    this.setState({ image: bigImg });
+  };
+
+  hideBigImg = () => {
+    this.setState({ image: '' });
+  };
+
+  render() {
+    const { imageList, loading, page, totalHits, image } = this.state;
+    return (
+      <>
+        <Searchbar formSubmit={this.formSubmit} />
+        {imageList.length > 0 && (
+          <ImageGallery imageList={imageList} showBigImg={this.showBigImg} />
+        )}
+
+        {loading && <Loader />}
+
+        {imageList.length > 0 && totalHits >= page * 12 && (
+          <ButtonLoadMore changePage={this.changePage} />
+        )}
+
+        {}
+        {image && <Modal bigPoster={image} hideBigImg={this.hideBigImg} />}
+      </>
+    );
+  }
+}
---
+++ src/services/images-api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/?';
+
+const KEY = '36182174-58580c701ddc81aa57ee59977';
+const perPage = 12;
+
+export async function fetchImages(sershString, page) {
+  const data = await axios({
+    params: {
+      q: sershString,
+      page,
+      key: KEY,
+      per_page: perPage,
+      orientation: 'horizontal',
+      image_type: 'photo',
+    },
+  });
+
+  return data;
+}
